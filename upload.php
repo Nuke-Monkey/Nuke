b7d65275b425c14859270e9d060449b8e493f944
Nuke-Monkey <126367570+Nuke-Monkey@users.noreply.github.com>
2023-02-26 12:51:32 -0500

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,8 +1,31 @@
 <?php
-  $FileName=$_FILES['fuResume']['name'];
-  $TmpName=FILES['fuResume']['tmp_name'];
-  
-  move_uploaded_file($TmpName,$FileName);
-  
-  echo("File Upload Succefful");
+  if (isset($_POST['submit'])) {
+    $file = $_FILES['file'];
+    
+    $fileName = $_FILES['file']['name'];
+    $fileTmpName = $_FILES['file']['tmp_name'];
+    $fileSize = $_FILES['file']['dize'];
+    $fileError = $_FILES['file']['error'];
+    $fileType = $_FILES['file']['type'];
+    
+    $fileExt = explode('.', $fileName);
+    $fileActualExt = strtolower(end($fileExt));
+    
+    $allowed = array('jpg', 'jpeg', 'png');
+    
+    if (in_array($fileActualExt, $allowed)) {
+      if ($fileError === 0) {
+        if ($fileSize < 500000) {
+          $fileNameNew = uniqid('', true).".".$fileActualExt;
+          $fileDestination = 'uploads/'.$fileNameNew;
+          move_uploaded_file($fileTmpName, $fileDestination);
+          header("Loacation: index.php?uploadsuccess")
+           
+        } else {
+            echo "The file is to large";
+      } else {
+          echo "There was an error uploading the file";
+        {
+    } else {
+      echo "That file type is invalid";
 ?>
